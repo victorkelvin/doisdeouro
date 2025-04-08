43698ad432099fe57c29fd818bee40f94488cd63
victorkelvin <victorkelvin@gmail.com>
2025-04-07 21:18:07 -0300

    `Removed redundant comments and modified serializer fields in atendimento/serializers.py`

--- backend/apps/atendimento/serializers.py
+++ backend/apps/atendimento/serializers.py
@@ -2,40 +2,32 @@ from rest_framework import serializers
 from apps.atendimento.models import Aula
 
 class AulaCreateSerializer(serializers.ModelSerializer):
-    """Serializer para criar aulas"""
     class Meta:
         model = Aula
         fields = ['id', 'data', 'aluno_presente', 'horario_inicio', 'horario_fim', 
                  'observacao', 'turma', 'instrutor']
     
     def create(self, validated_data):
-        """
-        Create and return a new Aula instance, given the validated data.
-        """
         aula = Aula.objects.create(**validated_data)
         return aula
 
 class AulaUpdateSerializer(serializers.ModelSerializer):
-    """Serializer para atualizar aulas"""
     class Meta:
         model = Aula
         fields = ['id', 'data', 'aluno_presente', 'horario_inicio', 'horario_fim', 
                  'observacao', 'turma', 'instrutor']
     
     def update(self, instance, validated_data):
-        """
-        Update and return an existing Aula instance, given the validated data.
-        """
+
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
         instance.save()
         return instance
 
 class AulaListSerializer(serializers.ModelSerializer):
-    """Serializer para listar aulas"""
     aluno_presente = serializers.StringRelatedField()
     turma = serializers.StringRelatedField()
-    instrutor = serializers.StringRelatedField(many=True)
+    instrutor = serializers.StringRelatedField()
     
     class Meta:
         model = Aula

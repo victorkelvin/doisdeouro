3255667fc59ade1e7b8b27852e94e8289642329a
victorkelvin <victorkelvin@gmail.com>
2025-04-08 00:06:31 -0300

    `Updated serializers and views for Aula and Instrutor models to use list syntax for field definitions`

--- backend/apps/atendimento/serializers.py
+++ backend/apps/atendimento/serializers.py
@@ -1,64 +1,77 @@
 from rest_framework import serializers
 from apps.atendimento.models import Aula
-# from apps.academia.models import Aluno
-# from apps.contas.models import Instrutor
+
 
 class AulaCreateSerializer(serializers.ModelSerializer):
-    # alunos_presentes = serializers.PrimaryKeyRelatedField(
-    #     many=True,
-    #     queryset=Aluno.objects.all(),
-    #     required=False
-    # )
-    # instrutores = serializers.PrimaryKeyRelatedField(
-    #     many=True,
-    #     queryset=Instrutor.objects.all(),
-    #     required=False
-    # )
 
     class Meta:
         model = Aula
-        fields = ['id', 'data', 'alunos_presentes', 'horario_inicio', 'horario_fim', 
-                 'observacao', 'turma', 'instrutores']
-    
+        fields = [
+            "id",
+            "data",
+            "alunos_presentes",
+            "horario_inicio",
+            "horario_fim",
+            "observacao",
+            "turma",
+            "instrutores",
+        ]
+
     def create(self, validated_data):
 
-        alunos_presentes = validated_data.pop('alunos_presentes', [])
-        instrutores = validated_data.pop('instrutores', [])
-        
+        alunos_presentes = validated_data.pop("alunos_presentes", [])
+        instrutores = validated_data.pop("instrutores", [])
+
         aula = Aula.objects.create(**validated_data)
-        
+
         aula.alunos_presentes.set(alunos_presentes)
         aula.instrutores.set(instrutores)
         return aula
 
 
-
 class AulaUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Aula
-        fields = ['id', 'data', 'alunos_presentes', 'horario_inicio', 'horario_fim', 
-                 'observacao', 'turma', 'instrutores']
-    
+        fields = [
+            "id",
+            "data",
+            "alunos_presentes",
+            "horario_inicio",
+            "horario_fim",
+            "observacao",
+            "turma",
+            "instrutores",
+        ]
+
     def update(self, instance, validated_data):
 
         for attr, value in validated_data.items():
-            if attr not in ['alunos_presentes', 'instrutores']:
+            if attr not in ["alunos_presentes", "instrutores"]:
                 setattr(instance, attr, value)
 
         instance.save()
-        if 'alunos_presentes' in validated_data:
-            instance.alunos_presentes.set(validated_data['alunos_presentes'])
-        
-        if 'instrutores' in validated_data:
-            instance.instrutores.set(validated_data['instrutores'])
+        if "alunos_presentes" in validated_data:
+            instance.alunos_presentes.set(validated_data["alunos_presentes"])
+
+        if "instrutores" in validated_data:
+            instance.instrutores.set(validated_data["instrutores"])
         return instance
+
 
 class AulaListSerializer(serializers.ModelSerializer):
     alunos_presentes = serializers.StringRelatedField()
     turma = serializers.StringRelatedField()
     instrutores = serializers.StringRelatedField()
-    
+
     class Meta:
         model = Aula
-        fields = ['id', 'data', 'alunos_presentes', 'horario_inicio', 'horario_fim', 
-                 'observacao', 'turma', 'instrutores']
+        fields = [
+            "id",
+            "data",
+            "alunos_presentes",
+            "horario_inicio",
+            "horario_fim",
+            "observacao",
+            "turma",
+            "instrutores",
+        ]
--- backend/apps/atendimento/views.py
+++ backend/apps/atendimento/views.py
@@ -4,37 +4,34 @@ from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.pagination import PageNumberPagination
 
 from .models import Aula
-from .serializers import (
-    AulaCreateSerializer,
-    AulaUpdateSerializer,
-    AulaListSerializer
-)
+from .serializers import AulaCreateSerializer, AulaUpdateSerializer, AulaListSerializer
+
 
 class CustomPagination(PageNumberPagination):
     page_size = 20
+
 
 class AulaViewSet(viewsets.ModelViewSet):
     queryset = Aula.objects.all()
     permission_classes = [IsAuthenticated]
     pagination_class = CustomPagination
-    
+
     def get_serializer_class(self):
-        if self.action == 'create':
+        if self.action == "create":
             return AulaCreateSerializer
-        elif self.action in ['update', 'partial_update']:
+        elif self.action in ["update", "partial_update"]:
             return AulaUpdateSerializer
         return AulaListSerializer
-    
+
     def get_permissions(self):
-        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+        if self.action in ["create", "update", "partial_update", "destroy"]:
             self.permission_classes = [IsAdminUser]
         return super().get_permissions()
-    
+
     def update(self, request, *args, **kwargs):
-        partial = kwargs.pop('partial', False)
+        partial = kwargs.pop("partial", False)
         instance = self.get_object()
         serializer = self.get_serializer(instance, data=request.data, partial=partial)
         serializer.is_valid(raise_exception=True)
         self.perform_update(serializer)
         return Response(serializer.data)
-

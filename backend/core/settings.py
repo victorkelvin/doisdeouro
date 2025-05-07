c58d8975fbb610a83c17d938060148d3d4327552
victorkelvin <victorkelvin@gmail.com>
2025-05-07 11:07:57 -0300

    `Added staticfiles directory to .gitignore and updated settings.py and urls.py to include static files settings.`

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -12,10 +12,14 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-from django.conf.urls.static import static
+# from django.conf.urls.static import static
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Static files settings
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/build')]
 
 # Media files settings
 MEDIA_URL = '/media/'
--- backend/core/urls.py
+++ backend/core/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('api/academia/', include(('apps.academia.urls' , 'apps.contas.urls'))),
     path('api/contas/', include(('apps.contas.urls'))),
     path('api/atendimento/', include(('apps.atendimento.urls'))),
-]
+] 
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT, )
